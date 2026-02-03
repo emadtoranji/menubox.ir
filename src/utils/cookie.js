2485cc53fde956e2463e0f66aede09f294236147
Emad <emadtoranji6@gmail.com>
2026-02-03 16:52:40 +0330

    improve cookie usage

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -5,30 +5,37 @@ export function getCookie(req, name) {
 
 export const cookieDefaultOptions = {
   path: '/',
-  sameSite: 'strict',
+  sameSite: process.env.NODE_ENV === 'development' ? 'lax' : 'strict',
   httpOnly: false,
   secure: process.env.NODE_ENV === 'production',
   maxAge: 60 * 60 * 24 * 365,
+  expires: new Date(Date.now() + 60 * 60 * 24 * 365 * 1000),
 };
 
-export function setCookie(response, name, value, options = {}) {
-  const {
-    path = cookieDefaultOptions.path,
-    sameSite = cookieDefaultOptions.sameSite,
-    httpOnly = cookieDefaultOptions.httpOnly,
-    secure = cookieDefaultOptions.secure,
-    maxAge = cookieDefaultOptions.maxAge,
-  } = options;
-
-  response.cookies.set({
-    name,
-    value,
+export function setCookie(response, options = {}) {
+  const { name, value, path, sameSite, httpOnly, secure, maxAge, expires } = {
+    ...cookieDefaultOptions,
+    ...options,
+  };
+  response.cookies.set(name, value, {
     path,
     sameSite,
     httpOnly,
     secure,
     maxAge,
+    expires,
   });
 
   return response;
 }
+
+export const getCookieDocument = (name) => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
+  return null;
+};
+
+export const setCookieDocument = (name, value, maxAgeSeconds) => {
+  document.cookie = `${name}=${value}; path=/; max-age=${maxAgeSeconds}; samesite=lax`;
+};

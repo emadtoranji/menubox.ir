630ae4d8c3a6f95d1f8ec1055231b6569d09b1d1
Emad <emadtoranji6@gmail.com>
2026-02-04 11:48:00 +0330

    remove unused variables

--- src/app/[lng]/(store)/(one-store)/store/[slug]/layout.jsx
+++ src/app/[lng]/(store)/(one-store)/store/[slug]/layout.jsx
@@ -1,4 +1,3 @@
-import { getT } from '@i18n/server';
 import Main from '@components/Main';
 
 export async function generateMetadata(props) {
@@ -6,9 +5,7 @@ export async function generateMetadata(props) {
   return m.generateMetadata(props, { forcedPage: 'store' });
 }
 
-export default async function Layout({ children, params }) {
-  const { lng } = (await params) || { lng: null };
-  const { t, lng: currentLang } = await getT(lng, 'header-footer');
+export default async function Layout({ children }) {
   return (
     <>
       <Main customClass={'store-section mb-5'}>{children}</Main>

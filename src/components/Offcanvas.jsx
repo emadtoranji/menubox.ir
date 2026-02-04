7467bc4cd006061886cf57c32dfa21f179886bf3
Emad <emadtoranji6@gmail.com>
2026-02-04 18:02:24 +0330

    init

---
+++ src/components/Offcanvas.jsx
@@ -0,0 +1,43 @@
+export function OffcanvasButton({
+  id = null,
+  btnTitle = 'Offcanvas',
+  btnClass = 'btn-primary',
+}) {
+  return (
+    <button
+      className={`btn ${btnClass}`}
+      type='button'
+      data-bs-toggle='offcanvas'
+      data-bs-target={`#${id}`}
+      aria-controls={id}
+    >
+      {btnTitle}
+    </button>
+  );
+}
+
+export function OffcanvasWrapper({ title, id = null, children }) {
+  return (
+    <div
+      className='offcanvas offcanvas-bottom'
+      tabIndex='-1'
+      id={id}
+      aria-labelledby={`${id}Label`}
+    >
+      <div className='offcanvas-header d-flex align-items-center justify-content-between'>
+        <h5 className='offcanvas-title' id={`${id}Label`}>
+          {title}
+        </h5>
+        <div>
+          <button
+            type='button'
+            className='btn-close'
+            data-bs-dismiss='offcanvas'
+            aria-label='Close'
+          ></button>
+        </div>
+      </div>
+      <div className='offcanvas-body'>{children}</div>
+    </div>
+  );
+}

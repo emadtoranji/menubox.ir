a2155e06d94dd9608b0f0386d7e3c88989328305
Emad <emadtoranji6@gmail.com>
2026-02-05 21:49:49 +0330

    change file name

---
+++ src/app/[lng]/(store)/(one-store)/store/[slug]/FreeSpanComponent.jsx
@@ -0,0 +1,9 @@
+import { useT } from '@i18n/client';
+
+export default function FreeSpanComponent({
+  additionalClass = 'text-success',
+}) {
+  const { t } = useT('store');
+
+  return <span className={`${additionalClass}`}>{t('free')}</span>;
+}
